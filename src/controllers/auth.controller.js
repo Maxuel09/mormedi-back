d8dca1b17e6ffa5771766467182b660a957d8f62
Maxuel09 <maxuelmorillocuevas@gmail.com>
2024-04-02 11:46:52 +0200

    new .envexample

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -25,6 +25,10 @@ const authController = {
                     profile: profile || "client"
                 }
             });
+
+            if (newUser) {
+                res.status(200).json({ message: 'User created successfully', newUser })
+            }
         } catch (error) {
             console.error("Error registering:", error);
             res.status(500).json({ message: 'Error registering', error })
