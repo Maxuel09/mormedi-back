bf3af643f0721caab7a5697af0409af2599ef6a0
Paolo Damen <paolodamen@gmail.com>
2024-04-04 09:50:13 +0200

    moved files

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import  Routes  from "express";
-import authController from "../controllers/auth.controller.js";
-
-const routesAuth = Routes();
-
-routesAuth.route('/login').post(authController.login)
-routesAuth.route('/register').post(authController.register)
-
-export default routesAuth;
