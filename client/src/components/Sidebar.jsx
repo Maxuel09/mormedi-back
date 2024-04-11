e02a3d320f08bcc6f2249121d995d91ee59359a0
Paolo Damen <paolodamen@gmail.com>
2024-04-11 19:31:07 +0200

    changes

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,38 @@
+import '../style/Sidebar.css'
+import Logo from '../assets/logo.svg'
+
+const Sidebar = () => {
+  return (
+    <div className='menu'>
+      <div className="logo">
+      <img src={Logo} alt="MORMEDI " />
+      </div>
+      <div className="menu--list">
+        <a href="#" className="item">
+          
+         home
+        </a>
+        <a href="#" className="item">
+          
+        customers
+        </a>
+        <a href="#" className="item">
+          
+        offers
+        </a>
+        <a href="#" className="item">
+          
+        reports
+        </a>
+        <a href="#" className="item">
+          
+        work team
+        </a>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
