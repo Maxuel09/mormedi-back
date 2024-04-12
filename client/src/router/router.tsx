752ebb0a227a2b66c3fda128ca161e655aa542a8
Paolo Damen <paolodamen@gmail.com>
2024-04-12 16:19:16 +0200

    changes

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Clients from '../pages/Clients';
-<<<<<<< HEAD
 import LoginForm from '../pages/LoginForm';
 import RegisterForm from "../pages/RegisterForm"
-
-||||||| cf9a058
-import Headless from '../pages/Headless'
-=======
 import Login from '../components/Login';
 import Root from './Root';
->>>>>>> 4f5c8d9f7ebfaf3904a681ec38cf7854d7734161
+
 
 const router = createBrowserRouter([
     {
@@ -27,7 +22,6 @@ const router = createBrowserRouter([
             }
         ],
     },
-<<<<<<< HEAD
     {
         path: "/login",
         element: <LoginForm />
@@ -36,16 +30,10 @@ const router = createBrowserRouter([
         path: "/register",
         element: <RegisterForm />
     },
-||||||| cf9a058
-    {
-        path: "/head",
-        element: <Headless />
-    },
     {
         path: "/register",
         element: <Login />
     },
-=======
     // {
     //     path: "/login",
     //     element: <Login />
@@ -54,7 +42,7 @@ const router = createBrowserRouter([
     //     path: "/register",
     //     element: <Register />
     // },
->>>>>>> 4f5c8d9f7ebfaf3904a681ec38cf7854d7734161
+
 ])
 
 export default router;
